c62a34bdfccec8108dbdc6bd772c4fe8229c714a
wangpeijian <519955464@qq.com>
2018-06-25 18:42:29 +0800

    修改

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
-docker build -t service-eureka  --build-arg port=8080  ./
+docker build -t service-eureka --build-arg port=8080  ./
 
 docker rmi java
 docker stop eureka9901
 docker rm eureka9901
 
-docker run -d --name="eureka9901" -p 9901:8080  service-eureka
+docker run -d --name="eureka9901" -p 9901:8080 service-eureka
