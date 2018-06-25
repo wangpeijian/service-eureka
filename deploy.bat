a4320a3bd563b9bb7f1f229c857a5ee27152028f
wangpeijian <519955464@qq.com>
2018-06-25 15:50:48 +0800

    修改

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 docker build -t service-eureka  --build-arg port=8080  ./
 
-docker run -d --name="eureka9901" -u="root" -p 9901:8080  service-eureka
-docker run -d --name="eureka9902" -u="root" -p 9902:8080  service-eureka
+docker run -d --name="eureka9901" -p 9901:8080  service-eureka
+docker run -d --name="eureka9902" -p 9902:8080  service-eureka
