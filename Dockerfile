68534faf53990b73a81fde409a7d6318c91cfcda
wangpeijian <519955464@qq.com>
2018-06-25 17:01:00 +0800

    修改

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ EXPOSE $port
 
 ADD ./build/libs/service-eureka-0.0.1-SNAPSHOT.jar /home/
 
-CMD ["java", "-jar", "/home/service-eureka-0.0.1-SNAPSHOT.jar"]
+CMD ["java", "-jar", "/home/service-eureka-0.0.1-SNAPSHOT.jar", "- -spring.profiles.active=$active"]
--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,8 @@
-docker build -t service-eureka  --build-arg port=8080  ./
+docker build -t service-eureka9901  --build-arg port=8080 active=eureka1 ./
+docker build -t service-eureka9902  --build-arg port=8080 active=eureka2 ./
 
 docker stop eureka9901 eureka9902
 docker rm eureka9901 eureka9902
 
-docker run -d --name="eureka9901" -p 9901:8080  service-eureka
-docker run -d --name="eureka9902" -p 9902:8080  service-eureka
+docker run -d --name="eureka9901" -p 9901:8080  service-eureka9901
+docker run -d --name="eureka9902" -p 9902:8080  service-eureka9902
