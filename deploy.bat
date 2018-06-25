fc837fc69ba4d819ba55935435fee65f13a466a1
wangpeijian <519955464@qq.com>
2018-06-25 17:13:18 +0800

    修改

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,6 @@
-docker build -t service-eureka9901  --build-arg active=eureka1 ./
-docker build -t service-eureka9902  --build-arg active=eureka2 ./
+docker build -t service-eureka  --build-arg port=8080  ./
 
-docker stop eureka9901 eureka9902
-docker rm eureka9901 eureka9902
+docker stop eureka9901
+docker rm eureka9901
 
-docker run -d --name="eureka9901" -p 9901:8080  service-eureka9901
-docker run -d --name="eureka9902" -p 9902:8080  service-eureka9902
+docker run -d --name="eureka9901" -p 9901:8080  service-eureka
