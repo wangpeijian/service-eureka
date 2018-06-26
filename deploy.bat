1459686412217365617c266d00e95257f8bf5f72
wangpeijian <519955464@qq.com>
2018-06-26 15:23:36 +0800

    修改

--- deploy.bat
+++ deploy.bat
@@ -5,4 +5,4 @@ docker build -t service-eureka --build-arg port=9901  ./
 docker stop eureka9901
 docker rm eureka9901
 
-docker run -i --name="eureka9901" -p 9901:9901 service-eureka
+docker run -d --name="eureka9901" -p 9901:9901 service-eureka
